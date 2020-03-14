1876176e808bb46af4c6647f46db2d9db3901d54
Paul David <paul@denknerd.org>
2020-03-14 11:38:35 +1100

    Add a conversion script.  To be removed later.
    Co-authored-by: Caitlin White <caitlinwhite@gmail.com>

---
+++ convert.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+def truncate(text, length = 100, truncate_string = '...')
+  if text
+    l = length - truncate_string.chars.length
+    (text.length > length ? text[0...l] + truncate_string : text).to_s
+  end
+end
+
+def proposed_filename(thing)
+  name = thing["name"]
+  truncate(name.downcase.gsub(/[^0-9a-z]/,'-'), 50) + ".yml"
+end
+
+require 'yaml'
+thing = YAML.load_file('old-company-data.yml')
+
+thing.each { |t|
+  puts t.inspect
+  puts proposed_filename(t)
+  destination = File.join("_data", "companies", proposed_filename(t))
+
+  File.open( destination, 'w' ) do |out|
+    YAML.dump( t, out )
+  end
+}
